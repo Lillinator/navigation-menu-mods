9b93e37d51a665dbb50b8a9d322c8379c0151e2f
Lilly <128197764+Lillinator@users.noreply.github.com>
2023-08-31 16:42:51 -0700

    Update group-navigation-menu.js

--- javascripts/discourse/components/group-navigation-menu.js
+++ javascripts/discourse/components/group-navigation-menu.js
@@ -1,1 +1,11 @@
 
+import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+import { computed } from "@ember/object";
+
+export default Component.extend({
+
+  bannerLinks: computed(function () {
+    return JSON.parse(settings.custom_menu_sections_groups);
+  }),
+});
