1dceb28ff331bcbfc94852fefc6780c9a3f2eb23
Lilly <128197764+Lillinator@users.noreply.github.com>
2023-08-31 18:11:39 -0700

    Update group-navigation-menu.js

--- javascripts/discourse/components/group-navigation-menu.js
+++ javascripts/discourse/components/group-navigation-menu.js
@@ -1,9 +1,24 @@
 import Component from "@ember/component";
-import discourseComputed from "discourse-common/utils/decorators";
-import { computed } from "@ember/object";
 
 export default Component.extend {
   get GroupNavigationMenus() {
     return JSON.parse(settings.custom_menu_sections_groups);
+  }
+});
+
+var groups = custom_menu_sections_groups.groups; 
+var menus = custom_menu_sections_groups.menu 
+
+groups.forEach(function(group) {
+  var sidebarSection = querySelector('.sidebar-section-wrapper.sidebar-section[data-section-name="' + menu + '"]');
+  if (sidebarSection) {
+     sidebarSection.style.display = 'none';
+     }  
+  var body = document.querySelector('body.group-' + group);
+  if (body) {
+    var sidebarSection = body.querySelector('.sidebar-section-wrapper.sidebar-section[data-section-name="' + menu + '"]');
+    if (sidebarSection) {
+      sidebarSection.style.display = 'block';
+    }
   }
 });
